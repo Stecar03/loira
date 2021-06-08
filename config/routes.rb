e267fa5c40dd34e442f1b6f565138e2c5a21310c
Stefano Carraro <s.carraro@donboscosandona.it>
2021-06-08 10:01:44 +0000

    index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :utentes
   resources :tappes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/home/index'
+  get '/index', to: 'home#index', as: 'index'
+
+  get '/home/tappes'
+  
+  get '/home/utentes'
 
   root 'home#index'
 end
